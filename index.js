a568320c8adfdf91cdf265377e47b7de4afc342a
NicoSchoeman <nico@celestial-games.com>
2019-12-05 20:38:33 +0200

    constrain to edges

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ import {
 } from "./systems.js";
 import Vector2 from "./vector2.js";
 
-export function force_graph () {
+export function force_graph (width = window.innerWidth, height = window.innerHeight, constrainInsideEdges = true) {
     this.world = new World();
+    this.world.constrainInsideEdges = constrainInsideEdges;
+    this.world.width = width;
+    this.world.height = height;
+    this.world.repulsion = 10;
+    this.world.attraction = 1;
+    this.world.spring = 50;
 
     this.world
         .registerSystem(AttractSystem)
--- systems.js
+++ systems.js
@@ -2,10 +2,8 @@ import { System, Not } from "./node_modules/ecsy/build/ecsy.module.js";
 import { Position, Velocity, Anchor, Link } from "./components.js";
 import Vector2 from "./vector2.js";
 
-let repulsion = 10;
-let attraction = 1;
-let spring = 50;
-
+//TODO: better proximity calculation
+//TODO: handle node inside node case
 export class AttractSystem extends System {
     execute (delta, time) {
         this.queries.links.results.forEach(linkEntity => {
@@ -22,7 +20,7 @@ export class AttractSystem extends System {
                 let distance = linkedNodePosition.value.distance(otherNodePosition.value);
                 let proximity = Math.max(distance, 1);
 
-                let force = attraction * (proximity - spring) / 100;
+                let force = this.world.attraction * (proximity - this.world.spring) / 100;
                 let vector = otherNodePosition.value.subtract(linkedNodePosition.value);
                 vector = vector.normalize();
                 vector = vector.scale(force);
@@ -49,22 +47,48 @@ export class RepulseSystem extends System {
                 return otherNode.id !== node.id
             });
 
-            let linkedNodePosition = node.getComponent(Position);
-            let linkedNodeVelocity = node.getMutableComponent(Velocity);
+            let nodePos = node.getComponent(Position);
+            let nodeVel = node.getMutableComponent(Velocity);
+
+            //top edge
+            let topEdge = nodePos.value.y < 10 ? nodePos.value.y - 10 : 0;
+            let vectorTop = this.calculateVector(nodePos.value, new Vector2(nodePos.value.x, topEdge));
+            nodeVel.value = nodeVel.value.add(vectorTop);
+
+            //bottom edge
+            let bottomEdge = nodePos.value.y > this.world.height - 10 ? nodePos.value.y + 10 : this.world.height;
+            let vectorBottom = this.calculateVector(nodePos.value, new Vector2(nodePos.value.x, bottomEdge));
+            nodeVel.value = nodeVel.value.add(vectorBottom);
+
+            //left edge
+            let leftEdge = nodePos.value.x < 10 ? nodePos.value.x - 10 : 0;
+            let vectorLeft = this.calculateVector(nodePos.value, new Vector2(leftEdge, nodePos.value.y));
+            nodeVel.value = nodeVel.value.add(vectorLeft);
+
+            //right edge
+            let rightEdge = nodePos.value.x > this.world.width - 10 ? nodePos.value.x + 10 : this.world.width;
+            let vectorRight = this.calculateVector(nodePos.value, new Vector2(rightEdge, nodePos.value.y));
+            nodeVel.value = nodeVel.value.add(vectorRight);
 
             allOtherNodes.forEach((otherNode) => {
                 let otherNodePosition = otherNode.getComponent(Position);
-                let distance = linkedNodePosition.value.distance(otherNodePosition.value);
-                let proximity = Math.max(distance, 1);
+                let vector = this.calculateVector(nodePos.value, otherNodePosition.value);
 
-                let force = -repulsion / proximity;
-                let vector = otherNodePosition.value.subtract(linkedNodePosition.value);
-                vector = vector.normalize();
-                vector = vector.scale(force);
-
-                linkedNodeVelocity.value = linkedNodeVelocity.value.add(vector);
+                nodeVel.value = nodeVel.value.add(vector);
             })
         });
+    }
+
+    calculateVector (myPos, otherPos) {
+        let distance = myPos.distance(otherPos);
+        let proximity = Math.max(distance, 1);
+
+        let force = -this.world.repulsion / proximity;
+        let vector = otherPos.subtract(myPos);
+        vector = vector.normalize();
+        vector = vector.scale(force);
+
+        return vector;
     }
 }
 RepulseSystem.queries = {
