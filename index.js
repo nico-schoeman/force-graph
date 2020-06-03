fb2c9d6e75d3dc02b99794d7e3452586438b6c43
NicoSchoeman <9889916+nico-schoeman@users.noreply.github.com>
2020-06-03 09:14:38 +0200

    ecsy imports

--- index.js
+++ index.js
@@ -1,70 +1,62 @@
-import { World } from "./node_modules/ecsy/build/ecsy.module.js";
-import { Position, Velocity, Link, Anchor } from "./components.js";
-import {
-    AttractSystem,
-    RepulseSystem,
-    MoveSystem
-} from "./systems.js";
-import Vector2 from "./vector2.js";
+import { World } from 'ecsy/build/ecsy.module.js';
+import { Position, Velocity, Link, Anchor } from './components.js';
+import { AttractSystem, RepulseSystem, MoveSystem } from './systems.js';
+import Vector2 from './vector2.js';
 
-export function force_graph (width = window.innerWidth, height = window.innerHeight, constrainInsideEdges = true) {
-    this.world = new World();
-    this.world.constrainInsideEdges = constrainInsideEdges;
-    this.world.width = width;
-    this.world.height = height;
-    this.world.repulsion = 1;
-    this.world.attraction = 1;
-    this.world.spring = 50;
-    this.world.radius = 500;
+export function force_graph(width = window.innerWidth, height = window.innerHeight, constrainInsideEdges = true) {
+	this.world = new World();
+	this.world.constrainInsideEdges = constrainInsideEdges;
+	this.world.width = width;
+	this.world.height = height;
+	this.world.repulsion = 1;
+	this.world.attraction = 1;
+	this.world.spring = 50;
+	this.world.radius = 500;
 
-    this.world
-        .registerSystem(AttractSystem)
-        .registerSystem(RepulseSystem)
-        .registerSystem(MoveSystem);
+	this.world
+		.registerSystem(AttractSystem)
+		.registerSystem(RepulseSystem)
+		.registerSystem(MoveSystem);
 
-    function animate() {
-        var time = performance.now();
-        var delta = time - lastTime;
-        this.world.execute(delta, time);
-        lastTime = time;
-        requestAnimationFrame(animate.bind(this));
-    }
+	function animate() {
+		var time = performance.now();
+		var delta = time - lastTime;
+		this.world.execute(delta, time);
+		lastTime = time;
+		requestAnimationFrame(animate.bind(this));
+	}
 
-    var lastTime = performance.now();
-    animate.bind(this)();
-    this.world.createEntity(); //Random entity to index properly
+	var lastTime = performance.now();
+	animate.bind(this)();
+	this.world.createEntity(); //Random entity to index properly
 }
 
 force_graph.prototype.addNode = async function(x, y) {
-    let newPos = new Vector2(
-        x || ((Math.random()-0.5)*0.1) + (window.innerWidth / 2),
-        y || ((Math.random()-0.5)*0.1) + (window.innerHeight / 2)
-    )
-    let node = await this.world
-        .createEntity()
-        .addComponent(Position, {
-            value: newPos
-        })
-        .addComponent(Velocity);
+	let newPos = new Vector2(
+		x || (Math.random() - 0.5) * 0.1 + window.innerWidth / 2,
+		y || (Math.random() - 0.5) * 0.1 + window.innerHeight / 2,
+	);
+	let node = await this.world
+		.createEntity()
+		.addComponent(Position, {
+			value: newPos,
+		})
+		.addComponent(Velocity);
 
-    node.makeAnchor = () => {
-      node.addComponent(Anchor);
-      return node;
-    }
+	node.makeAnchor = () => {
+		node.addComponent(Anchor);
+		return node;
+	};
 
-    return node;
-}
+	return node;
+};
 
 force_graph.prototype.addLink = async function(fromNode, toNode) {
-    let link = await this.world
-        .createEntity()
-        .addComponent(Link, { from: fromNode.id, to: toNode.id });
-    return link;
-}
+	let link = await this.world.createEntity().addComponent(Link, { from: fromNode.id, to: toNode.id });
+	return link;
+};
 
 force_graph.prototype.anchorNode = async function(node) {
-    node.addComponent(Anchor);
-    return node;
-}
-
-exports.force_graph = force_graph;
+	node.addComponent(Anchor);
+	return node;
+};
--- systems.js
+++ systems.js
@@ -1,119 +1,124 @@
-import { System, Not } from "./node_modules/ecsy/build/ecsy.module.js";
-import { Position, Velocity, Anchor, Link } from "./components.js";
-import Vector2 from "./vector2.js";
+import { System, Not } from 'ecsy/build/ecsy.module.js';
+import { Position, Velocity, Anchor, Link } from './components.js';
+import Vector2 from './vector2.js';
 
 export class AttractSystem extends System {
-    execute (delta, time) {
-        this.queries.links.results.forEach(linkEntity => {
-            let link = linkEntity.getComponent(Link);
-            let nodesLinked = this.queries.nodes.results.filter((node) => {return node.id === link.from || node.id === link.to});
-            nodesLinked.forEach((linkedNode) => {
-                let otherNode = nodesLinked.find((node) => {
-                    return node.id !== linkedNode.id;
-                });
+	execute(delta, time) {
+		this.queries.links.results.forEach(linkEntity => {
+			let link = linkEntity.getComponent(Link);
+			let nodesLinked = this.queries.nodes.results.filter(node => {
+				return node.id === link.from || node.id === link.to;
+			});
+			nodesLinked.forEach(linkedNode => {
+				let otherNode = nodesLinked.find(node => {
+					return node.id !== linkedNode.id;
+				});
 
-                let linkedNodePosition = linkedNode.getComponent(Position);
-                let otherNodePosition = otherNode.getComponent(Position);
+				let linkedNodePosition = linkedNode.getComponent(Position);
+				let otherNodePosition = otherNode.getComponent(Position);
 
-                let distance = linkedNodePosition.value.distance(otherNodePosition.value);
-                let proximity = Math.max(distance, 1);
+				let distance = linkedNodePosition.value.distance(otherNodePosition.value);
+				let proximity = Math.max(distance, 1);
 
-                let force = this.world.attraction * (proximity - this.world.spring) / 100;
-                let vector = otherNodePosition.value.subtract(linkedNodePosition.value);
-                vector = vector.normalize();
-                vector = vector.scale(force);
+				let force = (this.world.attraction * (proximity - this.world.spring)) / 100;
+				let vector = otherNodePosition.value.subtract(linkedNodePosition.value);
+				vector = vector.normalize();
+				vector = vector.scale(force);
 
-                let velocity = linkedNode.getMutableComponent(Velocity);
-                velocity.value = velocity.value.add(vector)
-            });
-        });
-    }
+				let velocity = linkedNode.getMutableComponent(Velocity);
+				velocity.value = velocity.value.add(vector);
+			});
+		});
+	}
 }
 AttractSystem.queries = {
-    nodes: {
-        components: [Position, Velocity]
-    },
-    links: {
-        components: [Link]
-    }
+	nodes: {
+		components: [Position, Velocity],
+	},
+	links: {
+		components: [Link],
+	},
 };
 
 export class RepulseSystem extends System {
-    execute (delta, time) {
-        this.queries.nodes.results.forEach((node) => {
-            let allOtherNodes = this.queries.nodes.results.filter((otherNode) => {
-                return otherNode.id !== node.id
-            });
+	execute(delta, time) {
+		this.queries.nodes.results.forEach(node => {
+			let allOtherNodes = this.queries.nodes.results.filter(otherNode => {
+				return otherNode.id !== node.id;
+			});
 
-            let nodePos = node.getComponent(Position);
-            let nodeVel = node.getMutableComponent(Velocity);
+			let nodePos = node.getComponent(Position);
+			let nodeVel = node.getMutableComponent(Velocity);
 
-            //top edge
-            let topEdge = nodePos.value.y < 10 ? nodePos.value.y - 10 : 0;
-            let vectorTop = this.calculateVector(nodePos.value, new Vector2(nodePos.value.x, topEdge));
-            nodeVel.value = nodeVel.value.add(vectorTop);
+			//top edge
+			let topEdge = nodePos.value.y < 10 ? nodePos.value.y - 10 : 0;
+			let vectorTop = this.calculateVector(nodePos.value, new Vector2(nodePos.value.x, topEdge));
+			nodeVel.value = nodeVel.value.add(vectorTop);
 
-            //bottom edge
-            let bottomEdge = nodePos.value.y > this.world.height - 10 ? nodePos.value.y + 10 : this.world.height;
-            let vectorBottom = this.calculateVector(nodePos.value, new Vector2(nodePos.value.x, bottomEdge));
-            nodeVel.value = nodeVel.value.add(vectorBottom);
+			//bottom edge
+			let bottomEdge = nodePos.value.y > this.world.height - 10 ? nodePos.value.y + 10 : this.world.height;
+			let vectorBottom = this.calculateVector(nodePos.value, new Vector2(nodePos.value.x, bottomEdge));
+			nodeVel.value = nodeVel.value.add(vectorBottom);
 
-            //left edge
-            let leftEdge = nodePos.value.x < 10 ? nodePos.value.x - 10 : 0;
-            let vectorLeft = this.calculateVector(nodePos.value, new Vector2(leftEdge, nodePos.value.y));
-            nodeVel.value = nodeVel.value.add(vectorLeft);
+			//left edge
+			let leftEdge = nodePos.value.x < 10 ? nodePos.value.x - 10 : 0;
+			let vectorLeft = this.calculateVector(nodePos.value, new Vector2(leftEdge, nodePos.value.y));
+			nodeVel.value = nodeVel.value.add(vectorLeft);
 
-            //right edge
-            let rightEdge = nodePos.value.x > this.world.width - 10 ? nodePos.value.x + 10 : this.world.width;
-            let vectorRight = this.calculateVector(nodePos.value, new Vector2(rightEdge, nodePos.value.y));
-            nodeVel.value = nodeVel.value.add(vectorRight);
+			//right edge
+			let rightEdge = nodePos.value.x > this.world.width - 10 ? nodePos.value.x + 10 : this.world.width;
+			let vectorRight = this.calculateVector(nodePos.value, new Vector2(rightEdge, nodePos.value.y));
+			nodeVel.value = nodeVel.value.add(vectorRight);
 
-            allOtherNodes.forEach((otherNode) => {
-                let otherNodePosition = otherNode.getComponent(Position);
-                let vector = this.calculateVector(nodePos.value, otherNodePosition.value);
+			allOtherNodes.forEach(otherNode => {
+				let otherNodePosition = otherNode.getComponent(Position);
+				let vector = this.calculateVector(nodePos.value, otherNodePosition.value);
 
-                nodeVel.value = nodeVel.value.add(vector);
-            })
-        });
-    }
+				nodeVel.value = nodeVel.value.add(vector);
+			});
+		});
+	}
 
-    calculateVector (myPos, otherPos) {
-        let distance = myPos.distance(otherPos);
-        if (distance > this.world.radius) return new Vector2(0, 0);
-        let proximity = Math.max(distance, 1);
+	calculateVector(myPos, otherPos) {
+		let distance = myPos.distance(otherPos);
+		if (distance > this.world.radius) return new Vector2(0, 0);
+		let proximity = Math.max(distance, 1);
 
-        let force = -this.world.repulsion / proximity;
-        let vector = myPos.x != otherPos.x && myPos.y != otherPos.y ? otherPos.subtract(myPos) : new Vector2(Math.random() - 0.5, Math.random() - 0.5);
-        vector = vector.normalize();
-        vector = vector.scale(force);
+		let force = -this.world.repulsion / proximity;
+		let vector =
+			myPos.x != otherPos.x && myPos.y != otherPos.y
+				? otherPos.subtract(myPos)
+				: new Vector2(Math.random() - 0.5, Math.random() - 0.5);
+		vector = vector.normalize();
+		vector = vector.scale(force);
 
-        return vector;
-    }
+		return vector;
+	}
 }
 RepulseSystem.queries = {
-    nodes: {
-        components: [Position, Velocity]
-    },
-    links: {
-        components: [Link]
-    }
-}
+	nodes: {
+		components: [Position, Velocity],
+	},
+	links: {
+		components: [Link],
+	},
+};
 
 export class MoveSystem extends System {
-    execute (delta, time) {
-        this.queries.nodes.results.forEach((node) => {
-            let position = node.getMutableComponent(Position);
-            let velocity = node.getMutableComponent(Velocity);
-            position.value = position.value.add(velocity.value.scale(delta));
-            velocity.value = velocity.value.scale(0);
-        })
-    }
+	execute(delta, time) {
+		this.queries.nodes.results.forEach(node => {
+			let position = node.getMutableComponent(Position);
+			let velocity = node.getMutableComponent(Velocity);
+			position.value = position.value.add(velocity.value.scale(delta));
+			velocity.value = velocity.value.scale(0);
+		});
+	}
 }
 MoveSystem.queries = {
-    nodes: {
-        components: [Position, Velocity, Not(Anchor)]
-    },
-    links: {
-        components: [Link]
-    }
-}
+	nodes: {
+		components: [Position, Velocity, Not(Anchor)],
+	},
+	links: {
+		components: [Link],
+	},
+};
